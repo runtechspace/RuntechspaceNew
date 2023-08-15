3b8174d674f0d56f9514f6516219aefdadc3eade
blakcode <emmanuelomolaja2@gmail.com>
2023-08-15 03:05:33 +0100

    remains only gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,6 +209,60 @@ document.addEventListener('scroll', toggleScrollTop);
     });
   });
 
+/**
+   * Porfolio isotope and filter
+   */
+window.addEventListener('load', () => {
+  let galleryContainer = select('.gallery-container');
+  if (galleryContainer) {
+    let galleryIsotope = new Isotope(galleryContainer, {
+      itemSelector: '.gallery-item'
+    });
+
+    let galleryFilters = select('#gallery-flters li', true);
+
+    on('click', '#gallery-flters li', function(e) {
+      e.preventDefault();
+      galleryFilters.forEach(function(el) {
+        el.classList.remove('filter-active');
+      });
+      this.classList.add('filter-active');
+
+      galleryIsotope.arrange({
+        filter: this.getAttribute('data-filter')
+      });
+      galleryIsotope.on('arrangeComplete', function() {
+        AOS.refresh()
+      });
+    }, true);
+  }
+
+});
+
+/**
+ * Initiate gallery lightbox 
+ */
+const galleryLightbox = GLightbox({
+  selector: '.gallery-lightbox'
+});
+
+/**
+ * gallery details slider
+ */
+new Swiper('.gallery-details-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
   /**
    * Init swiper sliders
    */
